3db155d3333a057355c35d04fc46e31e27dd1385
Gamer1650 <Gamer1650@GAMER1650>
2022-01-14 22:19:24 -0500

    Hw 16. fixed some errors. added comments

--- src/test/java/com/noorteck/java/hw16/BrokenCode16.java
+++ src/test/java/com/noorteck/java/hw16/BrokenCode16.java
@@ -1,55 +1,58 @@
 package com.noorteck.java.hw16;
 
 public class BrokenCode16 {
-	public static void mian(String[] args) 
+	public static void main(String[] args) //main spelled wrong
 	{
-		double addResult = addTwoNumbers(2.0, 4.0); //3.0
+		double addResult = addTwoNumbers(2.0, 4.0); //3.0. 
+		//We just need two numbers
 		
 		System.out.println(addResult);
 				
-		for(int i = 0; i < 10; i++)
-		{
-			System.out.println("Count" + i);
+		for(int i = 0; i < 10; i++)	// fixed errors with semicolons
+		{							// replaced = with <
+			System.out.println("Count" + i);	// replaced = with +
 		}
-		whoseFavNumber(23);//,1, true);
-		whoseFavNumber(3);//, true);	
+		
+		whoseFavNumber(23);//,1, true);		removed extra numbers
+		whoseFavNumber(3);//, true);		can only carry 1 number
 		whoseFavNumber(1);
 	
 		//String greeting = 
-				myName ("John Cena");
-	}
+				myName ("John Cena");	// void method. not returning value
+	}		// missing braces
 	public static void myName (String name) 
 	{
 			System.out.println("My name is " + name);
-	}
+	}// removed one braces
 	public static double addTwoNumbers(double  numOne, double numTwo)
-	{
-		double result = (numOne + numTwo);
+	{	// removed 'Void' , added double, added double variables instead of int
+		
+		double result = (numOne + numTwo);	// double, not int
 
 		System.out.println(numOne + " + " + numTwo + " = " + result);
 		
 		return result;
 	}
-	public static String whoseFavNumber(int i )
+	public static String whoseFavNumber(int i )	// not assigning value here
 	{
-		String result ="";
+		String result = "";	//String, not double
 		
-		if ( i == 1) 
+		if ( i == 1) // no ;
 		{
-			result = "Brook";
+			result = "Brook";	// in quotes with ; at the end
 		}
 		else if (i == 2 || i == 4 && i == 4) 
 		{
-			result = "Dannia";
+			result = "Dannia";		// result in lower case. Need double quotes
 		} 
-		else if (i == 3)
+		else if (i == 3)			// =, not is. no ;
 		{
 			result = "Helen";
 		}  
-		else
+		else			// need {
 		{
-			result = "my favorite number";
+			result = "my favorite number";	// double quotes
 		}
-		return result;
+		return result;		// this was missing. needed for no void methods
 	}
 }
